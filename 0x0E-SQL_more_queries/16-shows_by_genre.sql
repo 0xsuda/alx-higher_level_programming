5b7eb5f8a5c602b13fdcfeaa2f5731d88a106ebd
0xsuda <osama.suliman@protonmail.com>
2024-01-18 10:41:22 -0800

    normal

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,9 +5,10 @@
 USE hbtn_0d_tvshows;
 
 -- List all shows and their linked genres
-SELECT tv_shows.title, tv_genres.name
+SELECT tv_shows.title, GROUP_CONCAT(tv_genres.name ORDER BY tv_genres.name ASC) AS genre_names
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+GROUP BY tv_shows.title
+ORDER BY tv_shows.title ASC, genre_names ASC;
 
