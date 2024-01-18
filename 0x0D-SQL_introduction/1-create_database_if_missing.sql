e8bfa9ef808453684dd875ad0c817b22cf70c46c
0xsuda <osama.suliman@protonmail.com>
2024-01-17 20:24:53 -0800

    update

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
---Write a script that creates the database hbtn_0c_0 in your MySQL:
+-- Write a script that creates the database hbtn_0c_0 in your MySQL
 -- Connect to MySQL server (replace 'username' and 'password' with your credentials)
 -- mysql -u username -p
 
