0d39abc325cca4dd8b01b4536ce3a152402becf7
0xsuda <osama.suliman@protonmail.com>
2024-01-17 21:09:07 -0800

    10

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,1 +1,2 @@
-SELECT * FROM users WHERE age >= 10;
+-- List records with a score >= 10 from the table second_table ordered by score (top first)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
