7f6375e7ffe6c5efe852d4b10e4ef343e8f98452
0xsuda <osama.suliman@protonmail.com>
2024-01-17 20:44:26 -0800

    10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- Write sql script to show tables
-SHOW TABLES second_table;
+-- List all records from the table second_table ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
