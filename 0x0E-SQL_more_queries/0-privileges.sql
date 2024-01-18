4c73b728e44cbd924e2059e699f8a02c8fcc07b7
0xsuda <osama.suliman@protonmail.com>
2024-01-18 09:50:15 -0800

    more

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Connect to MySQL server
+-- mysql -u your_mysql_username -p
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
